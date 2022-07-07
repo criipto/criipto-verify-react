fe8044d0bd18d9b1c8644a0b9b36c08ac4be678e
Mick Hansen <maker@mhansen.io>
2022-07-07 12:34:05 +0200

    fix: expose useCriiptoVerify

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {default as CriiptoVerifyProvider} from './provider';
 import {default as AuthMethodSelector} from './components/AuthMethodSelector';
 import {default as AuthMethodButton} from './components/AuthMethodButton';
+import {default as useCriiptoVerify} from './use-criipto-verify';
 
-export {CriiptoVerifyProvider, AuthMethodSelector, AuthMethodButton};
+export {CriiptoVerifyProvider, AuthMethodSelector, AuthMethodButton, useCriiptoVerify};
