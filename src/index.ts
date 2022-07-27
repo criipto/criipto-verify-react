7d061704a044ad38939afef5104df9afc4f4ec85
Mick Hansen <maker@mhansen.io>
2022-07-27 16:41:10 +0200

    export filterAcrValues

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ import {default as QRCode} from './components/QRCode';
 import {default as useCriiptoVerify} from './use-criipto-verify';
 
 export {type Result} from './context';
+export {filterAcrValues} from './utils';
 export {OAuth2Error} from '@criipto/auth-js';
 export {CriiptoVerifyProvider, AuthMethodSelector, AuthMethodButton, useCriiptoVerify, QRCode};
