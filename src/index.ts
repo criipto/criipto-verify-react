2197e39db4093befefa8fb17b1d338e561a110ef
Mick Hansen <maker@mhansen.io>
2022-09-01 12:37:34 +0200

    export additional types

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import {default as AuthMethodButton} from './components/AuthMethodButton';
 import {default as QRCode} from './components/QRCode';
 import {default as useCriiptoVerify} from './use-criipto-verify';
 
-export {type Result} from './context';
-export {filterAcrValues} from './utils';
+export {type Result, type Action, actions} from './context';
+export {type Language, filterAcrValues} from './utils';
 export {OAuth2Error} from '@criipto/auth-js';
 export {CriiptoVerifyProvider, AuthMethodSelector, AuthMethodButton, useCriiptoVerify, QRCode};
