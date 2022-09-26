f8ec33461825ff243ca5c7fb9265bc0c313d34d6
Mick Hansen <maker@mhansen.io>
2022-09-26 15:03:47 +0200

    export SEBankIDQRCode

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import {default as CriiptoVerifyProvider} from './provider';
 import {default as AuthMethodSelector} from './components/AuthMethodSelector';
 import {default as AuthMethodButton} from './components/AuthMethodButton';
 import {default as QRCode} from './components/QRCode';
+import {default as SEBankIDQRCode} from './components/SEBankIDQRCode';
 import {default as useCriiptoVerify} from './use-criipto-verify';
 
 export {type Result, type Action, actions} from './context';
 export {type Language, filterAcrValues} from './utils';
 export {OAuth2Error} from '@criipto/auth-js';
-export {CriiptoVerifyProvider, AuthMethodSelector, AuthMethodButton, useCriiptoVerify, QRCode};
+export {CriiptoVerifyProvider, AuthMethodSelector, AuthMethodButton, useCriiptoVerify, QRCode, SEBankIDQRCode};
