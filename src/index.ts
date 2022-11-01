2d5cc7dab3c68a242bf983ac3764830c24ed46ab
Mick Hansen <maker@mhansen.io>
2022-11-01 14:14:49 +0100

    export AuthMethodSelectorSweden

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {default as CriiptoVerifyProvider} from './provider';
-import {default as AuthMethodSelector} from './components/AuthMethodSelector';
+import {default as AuthMethodSelector, Sweden as AuthMethodSelectorSweden} from './components/AuthMethodSelector';
 import {default as AuthMethodButton} from './components/AuthMethodButton';
 import {default as QRCode} from './components/QRCode';
 import {default as SEBankIDQRCode} from './components/SEBankIDQRCode';
@@ -8,4 +8,4 @@ import {default as useCriiptoVerify} from './use-criipto-verify';
 export {type Result, type Action, actions} from './context';
 export {type Language, filterAcrValues} from './utils';
 export {OAuth2Error} from '@criipto/auth-js';
-export {CriiptoVerifyProvider, AuthMethodSelector, AuthMethodButton, useCriiptoVerify, QRCode, SEBankIDQRCode};
+export {CriiptoVerifyProvider, AuthMethodSelector, AuthMethodSelectorSweden, AuthMethodButton, useCriiptoVerify, QRCode, SEBankIDQRCode};
